10d2b1a3a204d0142befcb26611bae02415b258e
AgentTroll <woodyc40@gmail.com>
2019-06-09 02:37:09 -0700

    More replica work

--- replica-spec/src/main/java/com/gmail/woodyc40/pbft/Transport.java
+++ replica-spec/src/main/java/com/gmail/woodyc40/pbft/Transport.java
@@ -10,6 +10,14 @@ import com.gmail.woodyc40.pbft.message.Request;
  */
 public interface Transport<T> {
     /**
+     * Obtains the current view number, used to determine
+     * the primary ID number.
+     *
+     * @return the current view number
+     */
+    int viewNumber();
+
+    /**
      * Called by the underlying layer upon receiving an
      * inbound {@link Request} message.
      *
@@ -26,4 +34,28 @@ public interface Transport<T> {
      * @param request the encoded request to send
      */
     void redirectRequest(int replicaId, T request);
+
+    /**
+     * Multicasts a PBFT {@code PRE-PREPARE} message in
+     * response to a request.
+     *
+     * @param prePrepare the encoded preprepare message
+     */
+    void multicastPrePrepare(T prePrepare);
+
+    /**
+     * Multicasts a PBFT {@code PREPARE} message in
+     * response to a pre-prepare message.
+     *
+     * @param prepare the encoded prepare message
+     */
+    void multicastPrepare(T prepare);
+
+    /**
+     * Multicasts a PBFT {@code COMMIT} message in response
+     * to a prepare message.
+     *
+     * @param commit the encoded commit message
+     */
+    void multicastCommit(T commit);
 }
